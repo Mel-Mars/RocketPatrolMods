96a33e5423ed2f87e221cec11ed5897180af350e
Mel Morris <mermorri@ucsc.edu>
2021-07-21 02:24:46 -0700

    Fixed Error Commit

--- src/main.js
+++ src/main.js
@@ -9,4 +9,4 @@ let game = new Phaser.Game(config);
 let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize / 3;
 // reserve keyboard vars
-let keyF, keyR, keyLEFT, key,RIGHT;
+let keyF, keyR, keyLEFT, keyRIGHT;
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -10,7 +10,7 @@ class Play extends Phaser.Scene {
     }
     create() {
         // place the tile sprite
-        this.starfiel = this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0,0);
+        this.starfield = this.add.tileSprite(0, 0, 640, 480, 'starfield').setOrigin(0,0);
         // green UI background
         this.add.rectangle(0, borderUISize + borderPadding, game.config.width, borderUISize * 2,  0x00FF00).setOrigin(0, 0);
         // white borders
@@ -20,7 +20,7 @@ class Play extends Phaser.Scene {
         this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
         // add rocket (p1)
         this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'rocket').setOrigin(0.5, 0);
-         // add spaceships (x3)
+        // add spaceships (x3)
         this.ship01 = new Spaceship(this, game.config.width + borderUISize*6, borderUISize*4, 'spaceship', 0, 30).setOrigin(0, 0);
         this.ship02 = new Spaceship(this, game.config.width + borderUISize*3, borderUISize*5 + borderPadding*2, 'spaceship', 0, 20).setOrigin(0,0);
         this.ship03 = new Spaceship(this, game.config.width, borderUISize*6 + borderPadding*4, 'spaceship', 0, 10).setOrigin(0,0);
