771fce5f3effaa88c5a134a5d94671ccc29bb505
Mel Morris <mermorri@ucsc.edu>
2021-07-22 16:36:08 -0700

    Fixing Errors Commit

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -12,7 +12,7 @@ class Menu extends Phaser.Scene {
     
     create() {
         let menuConfig = {
-            fontFamily: 'Courier', fontSize: '28px', backgroundColor: 'F3B141', color: '#843605', align: 'right', fixedWidth: 0, padding {
+            fontFamily: 'Courier', fontSize: '28px', backgroundColor: 'F3B141', color: '#843605', align: 'right', fixedWidth: 0, padding: {
                 top: 5,
                 bottom: 5,
             } 
@@ -20,7 +20,7 @@ class Menu extends Phaser.Scene {
         
         // show menu text
         this.add.text(game.config.width/2, game.config.height/2 - borderUISize - borderPadding, 'ROCKET PATROL', menuConfig).setOrigin(0.5);
-        this.add.text.(game.config.width/2, game.config.height/2, 'Use <--> arrowd to move & (F) to fire', menuConfig).setOrigin(0.5);
+        this.add.text.(game.config.width/2, game.config.height/2, 'Use <--> arrow to move & (F) to fire', menuConfig).setOrigin(0.5);
         menuConfig.backgroundColor = '#00FF00';
         menuConfig.color = '#000';
         this.add.text(game.config.width/2, game.config.height/2 + borderUISize + borderPadding, 'Press <- for Novice or -> for Expert', menuConfig).setOrigin(0.5);
