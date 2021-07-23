634e675573adfa8ed0607d44bcdf993767c9ae9c
Mel Morris <mermorri@ucsc.edu>
2021-07-22 23:05:41 -0700

    Fixing Errors

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -58,7 +58,7 @@ class Play extends Phaser.Scene {
 
         // 60=second play clock 
         scoreConfig.fixedWidth = 0;
-        this.clock = this.TimeRanges.delayedCall(game.settings.gameTimer, () => {
+        this.clock = this.time.delayedCall(game.settings.gameTimer, () => {
             this.add.text(game.config.width/2, game.config.height/2, 'GAME OVER', scoreConfig).setOrigin(0.5);
             this.add.text(game.config.width/2, game.config.height/2 + 64, 'Press (R) to Restart or <- for Menu', scoreConfig).setOrigin(0.5);  
                 this.gameOver = true;
@@ -118,7 +118,7 @@ class Play extends Phaser.Scene {
             boom.destroy(); // remove explosion sprite
         });
         // score add and repaint
-        thisp1Score += ship.points;
+        this.p1Score += ship.points;
         this.scoreLeft.text = this.p1Score;
         this.sound.play('sfx_explosion');
     }
