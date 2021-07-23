6f44c22bd9fd1cb6c56331aeafd90f2f6c830578
Mel Morris <mermorri@ucsc.edu>
2021-07-23 16:56:51 -0700

    Added comments

--- src/main.js
+++ src/main.js
@@ -11,3 +11,10 @@ let borderUISize = game.config.height / 15;
 let borderPadding = borderUISize / 3;
 // reserve keyboard vars
 let keyF, keyR, keyLEFT, keyRIGHT;
+
+// Name: Mel Morris
+// Project Title: Puppy Patrol
+// Date: 7/23/21
+// Time To Complete: About 12 hours
+// Points Breakdown:
+// Redesign the game's artwork, UI, and sound to change its theme/aesthetic (to something other than sci-fi) (60)
